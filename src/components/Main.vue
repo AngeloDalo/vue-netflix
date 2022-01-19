7cc1e02672eff3536fbbec10987b9a860df19eff
AngeloDalo <angelo251158@gmail.com>
2022-01-19 15:57:12 +0100

    commenti

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -53,11 +53,11 @@ export default {
     },
     data () {
         return {
-            flagFilm: [],
-            flagSerie: [],
-            cardFilms: null,
-            cardSeries: null,
-            filmName: 'no',
+            flagFilm: [], //link della bandiera per i film
+            flagSerie: [],  //link della bandiera per le serie
+            cardFilms: null, //contiene tutte le carte dei film
+            cardSeries: null, //contiene tutte le carte per le serie
+            filmSerieName: '', //film o serie inserito nell'input
             queryPathFILM: 'https://api.themoviedb.org/3/search/movie?api_key=c84442c7cda19b672849e1f2bfc0459d',
             queryPathTV: 'https://api.themoviedb.org/3/search/tv?api_key=c84442c7cda19b672849e1f2bfc0459d&query='
         }
@@ -68,15 +68,19 @@ export default {
     },
     methods: {
         searchFilm(event) {
-            this.filmName  = event;
+            //al filmSerieName andrà il valore inserito nell'input (event)
+            this.filmSerieName  = event;
             axios
             .get(this.queryPathFILM, {
                 params: {
-                    query: this.filmName
+                    //parametro dinamico del link
+                    query: this.filmSerieName
                 }
             })
             .then((response) => {
+                ///riempire le carte dei film con valori presi dal sito
                 this.cardFilms = response.data.results;
+                //per determinate lingue bisogna modificare il nome per trovare la bandiera
                 for (let i=0; i < this.cardFilms.length; i++) {
                     if (this.cardFilms[i].original_language == 'en') {
                         this.cardFilms[i].original_language = 'gb';
@@ -84,18 +88,21 @@ export default {
                     if (this.cardFilms[i].original_language == 'ja') {
                         this.cardFilms[i].original_language = 'jp';
                     }
+                    //nel fleg film andranno le iniziali della lingua
                     this.flagFilm.push(this.cardFilms[i].original_language);
+                    //creato un link dinamico per inserire la bandiera
                     this.flagFilm[i] = "https://www.bandiere-mondo.it/data/flags/w580/" + this.cardFilms[i].original_language + ".png" 
                 }
             })
             .catch(function (error) {
                 console.log(error);
             });
-
+            
+            //stessa cosa dei film fatta ora per le serie
             axios
             .get(this.queryPathTV, {
                 params: {
-                    query: this.filmName
+                    query: this.filmSerieName
                 }
             })
             .then((response) => {
