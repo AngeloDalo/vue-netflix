f67c2c8ed836c51dce3e4d0d074101caaee8a6ac
AngeloDalo <angelo251158@gmail.com>
2022-01-19 11:30:54 +0100

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+      main
+  </main>
+</template>
+
+<script>
+//import axios from 'axios';
+export default {
+    name: 'Main',
+    //metto elemento figlio
+    components: {
+    },
+    data () {
+        return {
+        }
+    },
+    computed: {
+    },
+    created() {
+        
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/_variables.scss";
+@import "../assets/scss/partials/_commons.scss";
+</style>
